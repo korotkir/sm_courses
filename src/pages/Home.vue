4fb8b080c84be73e2acc75ed2cde9273ce60a039
Koroteev Kirill <ekoroteev27@yandex.ru>
2024-10-24 17:59:08 +0300

    feat: add btn

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -11,14 +11,35 @@ const { assignedCourses } = storeToRefs(rootStore)
 </script>
 <template>
   <v-container>
-    <div class="title">
-      <h1>Назначенные курсы</h1>
+    <div class="top-block">
+      <div class="title">
+        <h1>Назначенные курсы</h1>
+      </div>
+      <v-btn
+        class="to-btn text-none text-subtitle-1"
+        color="#1976D2"
+        size="large"
+        variant="flat"
+      >
+        Назначить курсы
+      </v-btn>
     </div>
+
     <Courses :courseData="assignedCourses"/>
   </v-container>
 </template>
 
 <style lang="sass" scoped>
+.top-block
+  display: flex
+  flex-direction: row
+  justify-content: space-between
+  align-items: center
+  padding-bottom: 30px
+
+.to-btn
+  height: 100px
+
 .title h1
   padding: 15px 0
 </style>
